28f8aafd5c0319337f71c722a580b54357f8731f
박미나 <itmina04@gmail.com>
2026-09-16 18:05:43 +0900

    pair for 260916.ipynb

---
+++ mymodule.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+
+print('__name__', __name__)
+if __name__ == '__main__':
+    print(add(5, 2))
+    print(sub(5, 2))
